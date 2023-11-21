9cbeef0af8cd61c30a69b28cc6648b9d818293f8
Vítor Assis <contatoassisvt@gmail.com>
2023-11-21 20:12:56 -0300

    Update conta_aula.py

--- Aula-07_11_23/conta_aula.py
+++ Aula-07_11_23/conta_aula.py
@@ -2,35 +2,44 @@
   CEUB  -  FATECS  -  BCC  -  ADS  -  Programação  -  Prof. Barbosa
 ctlr<d>, duplica linha. ctrl<y>, apaga linha. ctrl</>, comenta linha.
 
-PEGAR O EXERCÍCIO NA SALA ON-LINE (MOODLE) - Aula 07/11
-
 - Implemente estes itens:
 
-- Crie três instâncias da subclasse Conta_PF com quatro, três e dois argumentos,
+1- Crie três instâncias da subclasse Conta_PF com quatro, três e dois argumentos,
 teste
-- Crie duas instâncias (objetos) da subclasse Conta_PJ com três e dois argumentos.
+2- Crie duas instâncias (objetos) da subclasse Conta_PJ com três e dois argumentos.
 Teste
-- Use alguns métodos das classes desenvolvidas
-- Faça um depósito, teste
-- Faça uma retirada, teste
+3- Use alguns métodos das classes desenvolvidas
+4- Faça um depósito, teste
+5- Faça uma retirada, teste
 
-- Refaça o método retirada usando RNs (Regras de Negócios) bancárias. Teste
-- A política do sistema bancário mudou, o depósito continua sem cobrado tarifa.
-    No saque, a pessoa física tem tarifa de 2 reais e pessoa juridica de 5 reais.
-    Então, substitua o método retirada da superclasse por dois métodos retirada nas
-    duas subclasse.
-- Teste a alteração anterior.
-- Crie o método retirada_rn2, substitua os dois método retirada nas duas subclasses
-   por um único método (retirada_rn2) na superclasse
+6- Refaça o método retirada usando RNs (Regras de Negócios) bancárias. Teste
+7- A política do sistema bancário mudou, o depósito continua sem cobrado tarifa.
+  No saque, a pessoa física tem tarifa de 2 reais e pessoa juridica de 5 reais.
+  Então, substitua o método retirada da superclasse por dois métodos retirada nas
+  duas subclasse.
+8- Teste a alteração anterior.
+9- Crie o método retirada_rn2, substitua os dois método retirada nas duas
+  subclasses por um único método (retirada_rn2) na superclasse
+
+- Crie um atributo de classe para armazenar a quantidade de contas (objetos) criados.
+    Onde colocar o contador (a atualização)?
+- Crie o método de classe get_qtd_conta.
+- Teste as alterações anteriores e o método de classe get_qtd_contas.
 
 """
 # O nome de classe começa com letra maiúscula e as outras letras minúsculas.
 # class Conta:
 # class Conta():
 class Conta(object):                    # Superclasse
+    qtd_conta = 0                   # Atributos de classe
+    @classmethod                      # Métodos de classe (decorator)
+    def get_qtd_conta(cls):
+        return cls.qtd_conta        # return cls.nomeAtributoClasse
+        # return Conta.qtd_conta    # return NomeClasse.nomeAtributoClasse
     def __init__(self, nome, saldo):  # Construtor  com default
         self.nome = nome                # Atributos de instância (objeto)
         self.saldo = saldo
+        Conta.qtd_conta += 1            # NomeClasse.atributoClasse
     def get_nome(self):                 # Consulta
         return self.nome
     def set_nome(self, novo_nome):      # Altera
@@ -38,19 +47,24 @@ class Conta(object):                    # Superclasse
     def get_saldo(self):
         return self.saldo
     def __str__(self):                  # Sobrescreve o método __str__ do Python
-        # s = 'Nome: ' + self.nome + ', R$ ' + str(self.saldo)  # Concatenação
         s = f'Nome: {self.nome}, R$ {self.saldo}'               # f-string
         return s
     def deposito(self, valor):          # Métodos funcionais
         self.saldo += valor             # self.saldo = self.saldo + valor
     def retirada(self, valor):          # Sem RN (Regra de Negócio)
         self.saldo -= valor             # self.saldo = self.saldo - valor
-
-
-# O nome de classe começa com letra maiúscula e as outras letras minúsculas.
-# class NomeSubclasse(NomeSuperclasse):     # Sintaxe
+    def retirada_rn2(self, valor):  # Retirada na superclasse
+        nome_classe = self.__class__.__name__
+        if nome_classe == 'Conta_PF':
+            total = valor + 2                  # RN de pessoa física
+        else:
+            total = valor + 5                  # RN de pessoa jurídica
+        if total > self.saldo :
+            print('Saldo insuficiente.')
+        else:
+            self.saldo -= total
 class Conta_PF(Conta):      # Subclasse Conta_PF herda da superclasse Conta
-    def __init__(self, nome, saldo, genero, cpf=''):  # Valores default
+    def __init__(self, nome, saldo, genero='', cpf=''):  # Valores default
         super().__init__(nome, saldo)   # Chama o construtor da superclasse
         self.genero = genero
         self.cpf = cpf
@@ -62,24 +76,9 @@ class Conta_PF(Conta):      # Subclasse Conta_PF herda da superclasse Conta
         return self.cpf
     def set_cpf(self, novo_cpf):
         self.cpf = novo_cpf
-
-
-""" - As principais modalidades de PJ:
-1 - MEI (Microempreendedor Individual)
-2 - ME – Microempresa
-3 - EPP (Empresa de Pequeno Porte)
-4 - EI (Empresário Individual) 
-5 - EIRELI (Empresa Individual de Responsabilidade Limitada)
-6 - Sociedade Limitada – LTDA
-7 - Sociedade Anônima (SA)                                  """
-
-# O nome de classe começa com letra maiúscula e as outras letras minúsculas.
-# class NomeSubclasse(NomeSuperclasse):   # Sintaxe
 class Conta_PJ(Conta):  # Subclasse Conta_PJ que herda da superclasse Conta
-    def __init__(self, nome, saldo, modalidade, cnpj=''):  # Construtor
+    def __init__(self, nome, saldo, modalidade='', cnpj=''):  # Construtor
         super().__init__(nome, saldo)       # Chama o construtor da superclasse
-        # super(Conta_PJ, self).__init__(nome, saldo)
-        # Conta.__init__(self, nome, saldo)
         self.modalidade = modalidade
         self.cnpj = cnpj
     def get_modalidade(self):
@@ -88,7 +87,24 @@ class Conta_PJ(Conta):  # Subclasse Conta_PJ que herda da superclasse Conta
         return self.cnpj
     def set_cnpj(self, novo_cnpj):
         self.cnpj = novo_cnpj
-
-
 if __name__ == '__main__':
-    ...
+    print("Qtd. contas:", Conta.get_qtd_conta())
+    # Método de classe (NomeClasse.nome_metodo())
+    pf1 = Conta_PF('Ana', 3000.0, 'f', '1234')  # Objeto da subclasse
+    print('- Pessoa Física 1:\nNome:', pf1.get_nome())  # Método na superclasse
+    pf2 = Conta_PF('Ailton', 7000.0, 'm')  # Objeto da subclasse Conta_PF
+    print(pf2)                          # Chama o método __str__()
+    print("Saldo:", pf2.get_saldo())
+    pf3 = Conta_PF('Alice', 6000.0)    # Objeto da subclasse Conta_PF
+    print(pf3)                          # Chama o método __str__()
+    print('CPF:', pf3.get_cpf())        # Método da subclasse
+    pj1 = Conta_PJ('Café ABC', 15000.0, 'MEI')  # objeto (instância) da subclasse
+    print(pj1)
+    print('- Pessoa Jurídica:\nNome:', pj1.get_nome())  # Método da superclasse
+    pj2 = Conta_PJ('Pão ABC', 17000.0)  # objeto (instância) da subclasse
+    print(pj2)
+    print("Saldo:", pj1.get_saldo())
+    pf1.retirada_rn2(10)                                     # Retirada com RN
+    print("- Pessoa Física 1, depois:\nSaldo:", pf1.get_saldo())    # Verifica
+    pj1.retirada_rn2(20)
+    print("- Pessoa Jurídica 1, depois:\nSaldo:", pj1.get_saldo())  # Verifica
