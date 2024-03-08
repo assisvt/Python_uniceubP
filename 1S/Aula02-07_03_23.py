2a3d52321e8e573a9425fcbdb253369c78cc11f5
Vítor Assis <contatoassisvt@gmail.com>
2024-03-08 10:07:40 -0300

    Create Aula02-07_03_23.py

---
+++ 1S/Aula02-07_03_23.py
@@ -0,0 +1,30 @@
+valor1 = int(input("Digite o primeiro valor: "))  #entrada de dados
+valor2 = int(input("Digite o segundo valor: "))
+# efetuar o cáuculo atribuindo o resultado à variável soma
+soma = valor1 + valor2 # Processamento
+print("A soma é igual a ", soma) # Saída
+# print(f'O resultado da soma é  {soma}') #Mostra o resultado
+--
+valor1 = int(input("Digite o primeiro valor: "))  #entrada de dados
+valor2 = int(input("Digite o segundo valor: "))
+# efetuar o cáuculo atribuindo o resultado à variável soma
+soma = valor1 + valor2 # Processamento
+print("A soma é igual a ", soma) # Saída
+# print(f'O resultado da soma é  {soma}') #Mostra o resultado
+subtração = valor1 - valor2
+print("A subtração é igual a ", subtração)
+multiplicacao = valor1 * valor2
+print("A multipicação é igual a", multiplicacao)
+soma_total = soma + subtração + multiplicacao
+print("A soma total dos valores lidos é", soma_total)
+--
+# Qual é a dose de água necessária de água em litros que uma pessoa deve tomar?
+# Sabe-se que é preciso multiplicar a massa em kg por 0,03. Elabore um programa que realize esse cálculo
+massa = float(input("Digite a massa: "))
+variavel = 0.03
+qtd_ideal = massa * variavel
+print(qtd_ideal)
+--
+f = float(input("Digite a temperatura em F: ")) #f= fahrenheit
+celsius = c = 5 * (f - 32)/9
+print(celsius)
